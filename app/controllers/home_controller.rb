38f01a6d716ea3ba150c9407ae32a07183cb2aa6
rbjarnason <robert@decyphermedia.com>
2012-06-09 15:32:31 -0400

    Merge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -40,26 +40,30 @@ class HomeController < ApplicationController
   end
 
   def world
-    @position_in_idea_name = true
-    @page_title = tr("{instance_name} Worldwide","shared/language_selection_master",:instance_name => tr(current_instance.name,"Name from database"))
-    @ideas = @world_ideas = Idea.unscoped.where(:sub_instance_id=>SubInstance.find_by_short_name("united-nations").id).published.top_rank.limit(3)
-    @eu_eea_ideas = Idea.unscoped.where(:sub_instance_id=>SubInstance.find_by_short_name("eu").id).published.top_rank.limit(3)
-    @country_sub_instance = SubInstance.where(:iso_country_id=>@iso_country.id).first if @iso_country
-    if @country_sub_instance
-      @country_sub_instance_ideas = Idea.unscoped.where(:sub_instance_id=>@country_sub_instance.id).published.top_rank.limit(3)
+    if request.subdomains.any? or request.subdomains[0] != 'www'
+      redirect_to "http://www.yrpri.org/home/world"
     else
-      @country_sub_instance_ideas = []
-    end
-    #@eu_eea_ideas = @country_sub_instance_ideas = @world_ideas = Idea.published.find(:all, :limit=>3, :order=>"rand()")
+      @position_in_idea_name = true
+      @page_title = tr("{instance_name} Worldwide","shared/language_selection_master",:instance_name => tr(current_instance.name,"Name from database"))
+      @ideas = @world_ideas = Idea.unscoped.where(:sub_instance_id=>SubInstance.find_by_short_name("united-nations").id).published.top_rank.limit(3)
+      @eu_eea_ideas = Idea.unscoped.where(:sub_instance_id=>SubInstance.find_by_short_name("eu").id).published.top_rank.limit(3)
+      @country_sub_instance = SubInstance.where(:iso_country_id=>@iso_country.id).first if @iso_country
+      if @country_sub_instance
+        @country_sub_instance_ideas = Idea.unscoped.where(:sub_instance_id=>@country_sub_instance.id).published.top_rank.limit(3)
+      else
+        @country_sub_instance_ideas = []
+      end
+      #@eu_eea_ideas = @country_sub_instance_ideas = @world_ideas = Idea.published.find(:all, :limit=>3, :order=>"rand()")
 
-    all_ideas = []
-    all_ideas += @country_sub_instance_ideas if @country_sub_instance_ideas
-    all_ideas += @world_ideas if @world_ideas
-    all_ideas += @eu_eea_ideas if @eu_eea_ideas
+      all_ideas = []
+      all_ideas += @country_sub_instance_ideas if @country_sub_instance_ideas
+      all_ideas += @world_ideas if @world_ideas
+      all_ideas += @eu_eea_ideas if @eu_eea_ideas
 
-    @endorsements = nil
-    if logged_in? # pull all their endorsements on the ideas shown
-      @endorsements = current_user.endorsements.active.find(:all, :conditions => ["idea_id in (?)", all_ideas.collect {|c| c.id}])
+      @endorsements = nil
+      if logged_in? # pull all their endorsements on the ideas shown
+        @endorsements = current_user.endorsements.active.find(:all, :conditions => ["idea_id in (?)", all_ideas.collect {|c| c.id}])
+      end
     end
   end
 

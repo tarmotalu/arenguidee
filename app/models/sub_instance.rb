42b5641867087f5abf1f65fa3f10f08651d46c46
Robert Bjarnason <rbjarnason>
2012-06-18 23:44:05 +0000

    Fixes for sub instance logos

--- app/models/sub_instance.rb
+++ app/models/sub_instance.rb
@@ -19,7 +19,7 @@ class SubInstance < ActiveRecord::Base
   validates_attachment_size :menu_strip_side, :less_than => 5.megabytes
   validates_attachment_content_type :menu_strip_side, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 
-  has_attached_file :logo, :styles => { :icon_96 => "96x96#", :icon_140 => "140x140#", :icon_340_74 => "340x74#", :icon_214_32 => "214x32#", :icon_107_16 => "107x16#", :icon_53_8 => "53x8#", :icon_180 => "180x180#", :medium  => "450x" }
+  has_attached_file :logo, :styles => { :icon_50 => "50x50#", :icon_96 => "96x96#", :icon_140 => "140x140#", :icon_340_74 => "340x74#", :icon_214_32 => "214x32#", :icon_107_16 => "107x16#", :icon_53_8 => "53x8#", :icon_180 => "180x180#", :medium  => "450x" }
     
   validates_attachment_size :logo, :less_than => 5.megabytes
   validates_attachment_content_type :logo, :content_type => ['image/jpeg', 'image/png', 'image/gif']

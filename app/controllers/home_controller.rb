f71ae3bdce213fca316786be09d1b6b0b57a8810
Robert Bjarnason <rbjarnason>
2012-08-14 15:40:15 +0000

    Fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -28,7 +28,7 @@ class HomeController < ApplicationController
     @position_in_idea_name = true
     if current_instance.domain_name.include?("yrpri") and (not request.subdomains.any? or request.subdomains[0] == 'www' and not params[:sub_instance_short_name])
       redirect_to :action=>"world"
-    elsif SubInstance.current.use_category_home_page
+    elsif SubInstance.current.use_category_home_page and SubInstance.current.use_category_home_page==true
       redirect_to :action=>"categories"
     else
       @page_title = SubInstance.current.name

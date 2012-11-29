4fa1a024295a6974e5664a92dc6dc273a9fe4b01
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2012-11-29 12:19:08 +0000

    Add banner links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121114141611) do
+ActiveRecord::Schema.define(:version => 20121129115845) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
@@ -689,6 +689,12 @@ ActiveRecord::Schema.define(:version => 20121114141611) do
     t.boolean  "hide_description",                            :default => false
     t.boolean  "hide_world_icon",                             :default => false
     t.integer  "idea_name_max_length",                        :default => 60
+    t.string   "left_link_url"
+    t.string   "right_link_url"
+    t.integer  "left_link_position",                          :default => 0
+    t.integer  "left_link_width",                             :default => 400
+    t.integer  "right_link_position",                         :default => 900
+    t.integer  "right_link_width",                            :default => 120
   end
 
   add_index "sub_instances", ["short_name"], :name => "short_name"

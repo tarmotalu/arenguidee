7a7ab05e03042056730575f5b9a0abae0a4505f4
Tarmo Talu <tarmo.talu@wisesoft.ee>
2012-12-29 17:08:07 +0200

    Moved mobile-id js into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,7 +34,6 @@
 //= require jquery.autoSuggest
 //= require jquery.tipsy
 //= require msdropdown
-//= require rahvakogu
 //= require_self
 //= require jquery.functions
 
@@ -135,3 +134,40 @@ function showSubNavLayer(layer) {
       myLayer.style.display="none";
     }
 }
+
+if (typeof Rahvakogu == "undefined" || !Rahvakogu) {
+    var Rahvakogu = {};
+}
+
+/* Mobile ID check */
+(function($) {
+    var checkInterval;
+    
+    var MobileIdCheck = function() {
+        return {
+            init: function($form) {
+                checkInterval = setInterval(function() {
+                    $.ajax({
+                        url: $form.attr('action'),
+                        type: $form.attr('method'),
+                        data: $form.serialize(),
+                        dataType: 'json',
+                        success: function(data, status, request) {
+                            if (data.message) {
+                                $form.find('.status').html(data.message);
+                            }
+                            if (data.notice) {
+                                $form.find('.status').html(data.notice);
+                            }
+                            if (data.redirect) {
+                                window.location.href = data.redirect;
+                            }
+                        }
+                    });
+                }, 8000);
+            }
+        };
+    }();
+    
+    Rahvakogu.MobileIdCheck = MobileIdCheck;
+})(jQuery);

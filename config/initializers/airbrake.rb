bcc15a7d51c2cd353a8de4f918ba1234e06008fb
John W. Fail <fail@cenotaph.org>
2013-02-20 23:18:35 +0200

    removing some configs

--- config/initializers/airbrake.rb
+++
@@ -1,3 +0,0 @@
-# Airbrake.configure do |config|
-#   config.api_key = Rails.application.config.database_configuration[Rails.env]["airbrake_api_key"]
-# end
--- config/initializers/recaptcha.rb
+++
@@ -1,4 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6Lc6d9ISAAAAAO59NBKpyWsY6q3r4TJ1eS2pWpme'
-  config.private_key = '6Lc6d9ISAAAAAArkHk3fSxmy8P5RHF7zDa7A0sj6'
-end

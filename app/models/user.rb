3d4d5ba4922ca965c6606c77c71bc045adae0384
John W. Fail <fail@cenotaph.org>
2013-06-06 15:44:51 +0300

    more javascript fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,10 +80,10 @@ class User < ActiveRecord::Base
     
   has_many :point_qualities, :dependent => :destroy
 
-  has_many :votes, :dependent => :destroy
+  # has_many :votes, :dependent => :destroy
 
   has_many :comments, :dependent => :destroy
-  has_many :blasts, :dependent => :destroy
+  # has_many :blasts, :dependent => :destroy
   has_many :ads, :dependent => :destroy
   has_many :shown_ads, :dependent => :destroy
   has_many :charts, :class_name => "UserChart", :dependent => :destroy

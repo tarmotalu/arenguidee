55fae7b4274ce84e7de8e47484e9e56bddaa9ca8
Andri Möll <andri@dot.ee>
2014-02-13 00:16:23 +0200

    Filter more test backtraces.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,10 @@ require "rails/test_help"
 require "minitest/rails"
 require "minitest/pride"
 require "minitest/reporters"
+
 MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
+Minitest.backtrace_filter.add_filter %r(/zeus-/)
+Minitest.backtrace_filter.add_filter %r(/\.gem/)
 
 class ActiveSupport::TestCase
   fixtures :all
